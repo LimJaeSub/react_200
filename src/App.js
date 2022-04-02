db71e104d556ca9f1bd2bb78685df45e9ca0c8b4
LimJaeSub <76646095+LimJaeSub@users.noreply.github.com>
2022-04-03 03:19:17 +0900

    dfs
    sdf

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ImportComponent from './R03_importComponent';
+
+function App() {
+  return (
+    <div className="App">
+        <ImportComponent></ImportComponent>
+    </div>
+  );
+}
+
+export default App;
